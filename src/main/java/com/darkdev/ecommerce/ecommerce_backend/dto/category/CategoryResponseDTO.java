ed2f4c1c6e16c19ee19ae18d1d9749bce2ff2b35
redrum <franco.avalos@live.com>
2025-04-07 17:06:10 -0400

    feat: add url to categories

--- src/main/java/com/darkdev/ecommerce/ecommerce_backend/dto/category/CategoryResponseDTO.java
+++ src/main/java/com/darkdev/ecommerce/ecommerce_backend/dto/category/CategoryResponseDTO.java
@@ -2,9 +2,11 @@ package com.darkdev.ecommerce.ecommerce_backend.dto.category;
 
 public class CategoryResponseDTO {
     private String name;
+    private String url;
 
-    public CategoryResponseDTO(String name) {
+    public CategoryResponseDTO(String name, String url) {
         this.name = name;
+        this.url = url;
     }
 
     public String getName() {
@@ -13,5 +15,13 @@ public class CategoryResponseDTO {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
